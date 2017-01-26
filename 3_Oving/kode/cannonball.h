7c8e2313e3a08caad1b4b0e3d8fdc038a9867044
Mathias H <mathias.moe@gmail.com>
2017-01-26 01:45:40 +0100

    Starter Øving 3

---
+++ 3_Oving/kode/cannonball.h
@@ -0,0 +1,9 @@
+double acclY();
+double velY(double initVelocityY, double time);
+
+double posX(double initPosition, double initVelocity, double time);
+double posY(double initPosition, double initVelocity, double time);
+
+void printTime(int time);
+
+double flightTime(double initVelocity);
---
+++ 3_Oving/kode/main.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+#include "cannonball.h"
+
+int main() {
+    return 0;
+}
